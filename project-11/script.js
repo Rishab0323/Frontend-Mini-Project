987b28df5a7bfdf96c584cc4cf9f126a1162a3a3
Rishab0323 <srishbh574@gmail.com>
2025-01-31 23:03:24 +0530

    key event done

--- project-11/script.js
+++ project-11/script.js
@@ -1,3 +1,18 @@
+const insert = document.getElementById('insert');
+
 window.addEventListener('keydown',(e) => {
-    console.log(e);
+    insert.innerHTML = ` 
+    <div class="key">
+            ${event.key === ' ' ? 'space' : event.key} 
+            <small>event.key</small>
+        </div>
+        <div class="key">
+            ${event.keyCode}
+            <small>event.key</small>
+        </div>
+        <div class="key">
+            ${event.code}
+            <small>event.key</small>
+        </div>`
 })
+
