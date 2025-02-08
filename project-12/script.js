4a966dbe84b24c33d001da50801ea11da9a351e5
Rishab0323 <srishbh574@gmail.com>
2025-02-08 23:56:25 +0530

    js file

---
+++ project-12/script.js
@@ -0,0 +1,8 @@
+const toggles = documents.querySelectorAll('faq-toggle')
+
+toggles.forEach(toggles => {
+    toggles.addEventListener('click',() => {
+        toggles.parentNpde.clasList.tooggle
+        ('active')
+    })
+})
