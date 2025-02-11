b6ffefe9f019ab14a2581de15bbec8794cd2360c
Rishab0323 <srishbh574@gmail.com>
2025-02-11 23:33:21 +0530

    id and class error

--- project-13/script.js
+++ project-13/script.js
@@ -12,4 +12,13 @@ function createTags(input){
 !== '').map(tag => tag.trim())
 
 
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl);
+    })
+
 }
