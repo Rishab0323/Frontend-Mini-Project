55164afc28e792bc460d64c2352bf3ac9650fcbb
Rishab0323 <srishbh574@gmail.com>
2025-08-22 23:52:02 +0530

    learning react basic

---
+++ react/proj/mini-app/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '/.App.css';
+const App = () => {
+  return(
+    
+    <div className="contact card">
+      <img alt="profile">
+      </img>
+
+    <div className="user-details">
+      <p>Nmae : Satyam Kumar Singh</p>
+      <p>Email: satyam@420gmail.com</p>
+      <p>age: Immortal </p>
+    </div>
+    </div>
+    
+  )
+}
+
+
+export default App;
---
+++ react/proj/mini-app/src/contactCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContactCard  = {} => {
+    return(
+        <p>return the image of the student</p>
+    )
+}
+
+export default ContactCard;
