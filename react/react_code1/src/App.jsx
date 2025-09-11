34cd36ca155e31d79c68dbb42c6a39144a512e45
Rishab0323 <srishbh574@gmail.com>
2025-09-11 23:54:25 +0530

    contact card with react

--- react/react_code1/src/App.jsx
+++ react/react_code1/src/App.jsx
@@ -2,41 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ContactCard from './contact-card.jsx'
 
-// function handle(){
-//   alert("Hi Rishab");
-// }
+
 
 function App() {
-
-  const [todos,setTodos] = useState([{
-    title: "Go to gym",
-    description: "focus fitnes",
-    complete: "false"
-  },
-  {
-    title: "Go to gym",
-    description: "focus fitnes",
-    complete: "false"
-  }
-]);
-
   return (
-      <div className="card">
-       <Todo title={todos[0].title} description={todos[0].description}></Todo>
-       <Todo title={title[1].title} description={todos[1].description}></Todo>
-      </div>
-      
-  )  
-}
-
-function Todo(prop){
-  return (<div>
-    <h3>title.prop</h3>
-    <head>description.prop</head>
-  </div>
+    <div>
+      <ContactCard/>
+      <ContactCard/>
+    </div>
+    
   )
 }
+
 
 export default App
  
---
+++ react/react_code1/src/contact-card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ContactCard = () => {
+
+    return (
+    <div className="contact-card" >
+    <img src="sc1.png" alt="profile"></img>
+    <div className="user-detail">
+      <p>Name: Rishab Shukla</p>
+      <p>Email: srishbcs.12</p>
+      <p>Dept: CSE</p>
+    </div>
+  </div>
+  )
+}
+
+export default ContactCard;
