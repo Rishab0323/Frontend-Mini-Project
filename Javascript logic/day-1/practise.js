32321cfce58c9d4b98c34b9e41238effcb9df766
Rishab0323 <srishbh574@gmail.com>
2025-12-21 23:04:58 +0530

    JS logic ready

---
+++ Javascript logic/day-1/practise.js
@@ -0,0 +1,16 @@
+let myName = 'Rishab Shukla'
+console.log(myName.length);
+console.log(myName[3])
+
+let firstName = 'Rishab'
+console.log(firstName[0]);
+
+let concateNum = firstName + 2003
+console.log(concateNum);
+
+let emptyString = '';
+let stringWithSpace = ' ' ;
+
+console.log(emptyString.length);
+console.log(stringWithSpace.length);
+console.log(stringWithSpace[0]);
