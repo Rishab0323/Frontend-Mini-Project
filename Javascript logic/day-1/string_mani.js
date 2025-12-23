66eb6de78ab05ee930c28f73fd4f82db7a3387ea
Rishab0323 <srishbh574@gmail.com>
2025-12-23 20:06:05 +0530

    string

--- Javascript logic/day-1/string_mani.js
+++ Javascript logic/day-1/string_mani.js
@@ -17,3 +17,12 @@ let arr1 = [1,2,3,4,5]
 let n= 3
 console.log(arr1.splice(0,n));
 console.log(arr1.slice(arr1.length-2))
+
+let name1 = 'as';
+
+if(name1[0] === 'a' && name1.length >= 3 ){
+    console.log("Is a good string");
+}
+else{
+    console.log('not a good string');
+}
