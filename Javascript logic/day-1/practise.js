c4dc566ec3b3f45506ac5159a68e8c5ace3d3387
Rishab0323 <srishbh574@gmail.com>
2025-12-23 09:42:52 +0530

    array mani

--- Javascript logic/day-1/practise.js
+++ Javascript logic/day-1/practise.js
@@ -1,50 +1,58 @@
-let myName = 'Rishab Shukla'
-console.log(myName.length);
-console.log(myName[3])
+// let myName = 'Rishab Shukla'
+// console.log(myName.length);
+// console.log(myName[3])
 
-let firstName = 'Rishab'
-console.log(firstName[0]);
+// let firstName = 'Rishab'
+// console.log(firstName[0]);
 
-let concateNum = firstName + 2003
-console.log(concateNum);
+// let concateNum = firstName + 2003
+// console.log(concateNum);
 
-let emptyString = '';
-let stringWithSpace = ' ' ;
+// let emptyString = '';
+// let stringWithSpace = ' ' ;
 
-console.log(emptyString.length);
-console.log(stringWithSpace.length);
-console.log(stringWithSpace[0]);
+// console.log(emptyString.length);
+// console.log(stringWithSpace.length);
+// console.log(stringWithSpace[0]);
 
-//String methods
+// //String methods
 
-let name1 = 'Javascript king';
-console.log(name1.toLowerCase());
-console.log(name1.toUpperCase());
-console.log(name1.indexOf('s'));
+// let name1 = 'Javascript king';
+// console.log(name1.toLowerCase());
+// console.log(name1.toUpperCase());
+// console.log(name1.indexOf('s'));
 
-console.log(name1.slice(0,4));
-console.log(name1.replace('hello',' is webdev king'));
-console.log(name1.repeat(3));
+// console.log(name1.slice(0,4));
+// console.log(name1.replace('hello',' is webdev king'));
+// console.log(name1.repeat(3));
 
-let arr = ['audi','bike',12,'hello'];
-// arr.push('ferrari');
-arr.pop();
-arr.shift();
-arr.unshift('dhan')
-console.log(arr);
+// let arr = ['audi','bike',12,'hello'];
+// // arr.push('ferrari');
+// arr.pop();
+// arr.shift();
+// arr.unshift('dhan')
+// console.log(arr);
 
-let month = ['january','july','march','august']
-month.shift()
-month.shift()
-console.log(month)
-month.unshift('july');
-month.unshift('june');
-month[0].replace('april') 
-console.log(month);
+// let month = ['january','july','march','august']
+// month.shift()
+// month.shift()
+// console.log(month)
+// month.unshift('july');
+// month.unshift('june');
+// month[0].replace('april') 
+// console.log(month);
 
-let cars = ['audi','bmw','safari','tata'];
-console.log(cars.slice());
-console.log(cars.slice(2));
-console.log(cars.slice(-3))
-console.log(cars.slice(-2,1))
-console.log(cars.slice(2,4))
+// let cars = ['audi','bmw','safari','tata'];
+// console.log(cars.slice());
+// console.log(cars.slice(2));
+// console.log(cars.slice(-3))
+// console.log(cars.slice(-2,1))
+// console.log(cars.slice(2,4))
+
+let colors = ['red','yellow','blue','green','white'];;
+// console.log(colors.splice(4));
+// console.log(colors.splice(-3));
+
+console.log(colors.splice(1,0,"read"));
+console.log(colors);
+
--- Javascript logic/day-1/string_mani.js
+++ Javascript logic/day-1/string_mani.js
@@ -1,5 +1,19 @@
 let month = ['january','july','march','august'];
 console.log(month.shift());
-console.log(month);
 console.log(month.splice(0,1,'july','june'));
 console.log(month)
+
+//array reference 
+let arr = ['hi','hel']
+let arrCopy = arr
+console.log(arr == arrCopy);
+let num = ['2'] ;
+let num1 = ['2'];
+console.log(num == num1);
+console.log(num === num1);
+
+//get first n element of an array
+let arr1 = [1,2,3,4,5]
+let n= 3
+console.log(arr1.splice(0,n));
+console.log(arr1.slice(arr1.length-2))
