b37d44ad0f879bab34eae64c55d3aecd24cbd9ad
Rishab0323 <srishbh574@gmail.com>
2025-12-31 08:33:32 +0530

    enum in ts

--- Typescript/a.ts
+++ Typescript/a.ts
@@ -1,23 +1,65 @@
-let x: number = 12;
-console.log(x);
+// let x: number = 12;
+// console.log(x);
 
-function greet(firstName : string){
-    console.log('helllo' +firstName);
+// function greet(firstName : string){
+//     console.log('helllo' +firstName);
+// }
+
+// function Sum(x:number,y:number): number{
+//     return x+y;
+// }
+
+// interface User{
+//     firstName:string,
+//     lastName:string,
+//     age:number,
+//     email?:string   //? indicate any type
+// };
+
+// function great(user : User){
+//     console.log('hi there ' +user.firstName);
+// }
+
+// function check(age : number) : boolean{
+//     if(age > 18){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// console.log(Sum(1,3));
+// greet("Rishab");
+// console.log(check(20))
+
+//enum they are 
+//we assign random value to enum but each and everyone
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
 }
 
-function Sum(x:number,y:number): number{
-    return x+y;
-}
+function doSomething(keyPressed:Direction){
+    if(keyPressed == Direction.Left){
 
-function check(age : number) : boolean{
-    if(age > 18){
-        return true;
     }
-    else{
-        return false;
-    }
 }
 
-console.log(Sum(1,3));
-greet("Rishab");
-console.log(check(20))
+console.log(doSomething(Direction.Left))
+console.log(doSomething(Direction.Down));
+
+// they are useful sending clear message like status code
+//like success code = 200 ,then we can write re
+
+// enum ResponseStatus {
+//     Succes = 200,
+//     NotFound = 404,
+//     Error = 500
+// }
+
+// app.get("/",(req,res) => {
+//     res.status(ResponseStatus.NotFound).json({}) //clean code using enum and understandable
+// })
